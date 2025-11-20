91b8130b6fe8bdbbe0f8bc09e5dd68c212c19820
KIREEVDANIIL <146159768+KIREEVDANIIL@users.noreply.github.com>
2025-11-20 10:24:07 +0500

    register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,13 +1,10 @@
 <x-layout title="Регистрация">
     <x-section title="Регистрация">
-        <!-- Сообщения об успехе -->
         @if(session('success'))
         <div class="mb-6 p-4 bg-green-100 border border-green-400 text-green-700 rounded">
             {{ session('success') }}
         </div>
         @endif
-
-        <!-- Сообщения об ошибках -->
         @if($errors->any())
         <div class="mb-6 p-4 bg-red-100 border border-red-400 text-red-700 rounded">
             <ul class="list-disc list-inside">
